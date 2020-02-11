7588b33508c830715e8ae328c7ba55e57d22b643
Alberto Basalo <albertobasalo@hotmail.com>
2020-02-11 20:22:59 +0100

    🎢: improved ways of arrange with beforeall

--- src/tdd/tasks/hamming-dna.spec.js
+++ src/tdd/tasks/hamming-dna.spec.js
@@ -10,19 +10,56 @@ So:         I can see how many mistakes occurred.
 */
 
 // Scenario: we have two valid sequences of equal length
+
+describe('GIVEN: a Hamming Calculator that receives two valid strings', () => {
+  // Arrange
+  const sut = new HammingCalculator();
+  describe('WHEN: I compare two empty strands', () => {
+    const input = ['', ''];
+    // Act
+    const actual = sut.compare(...input);
+    test('THEN: should return cero', () => {
+      const expected = 0;
+      // assert
+      expect(actual).toEqual(expected);
+    });
+  });
+});
+
+// Alternate ways
+
 describe('GIVEN: a Hamming Calculator that receives two valid strings', () => {
   let sut;
   beforeAll(() => {
     // Arrange
     sut = new HammingCalculator();
-    console.log('1 - beforeAll');
   });
   describe('WHEN: I compare two empty strands', () => {
     test('THEN: should return cero', () => {
       const input = ['', ''];
       // Act
-      console.log('2 - WHEN');
       const actual = sut.compare(...input);
+      const expected = 0;
+      // assert
+      expect(actual).toEqual(expected);
+    });
+  });
+});
+
+describe('GIVEN: a Hamming Calculator that receives two valid strings', () => {
+  let sut;
+  let actual;
+  beforeAll(() => {
+    // Arrange
+    sut = new HammingCalculator();
+  });
+  describe('WHEN: I compare two empty strands', () => {
+    beforeAll(() => {
+      const input = ['', ''];
+      // Act
+      actual = sut.compare(...input);
+    });
+    test('THEN: should return cero', () => {
       const expected = 0;
       // assert
       expect(actual).toEqual(expected);
