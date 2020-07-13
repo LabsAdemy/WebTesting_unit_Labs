de752e1837aec2faea736af90a3ad8daedf4b085
Alberto Basalo <albertobasalo@hotmail.com>
2020-07-13 14:17:37 +0200

    video tdd

--- src/2-tdd/0-bank-client.spec.js
+++
@@ -1,7 +0,0 @@
-import { BankClient } from './0-bank-client';
-describe(`GIVEN a Bank Client class`, () => {
-  test(`THEN I can create an instance`, () => {
-    const sut = new BankClient();
-    expect(sut).toBeInstanceOf(BankClient);
-  });
-});
