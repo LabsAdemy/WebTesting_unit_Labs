d722f666922f4d11e3b97b65d40d9faf249bc854
Alberto Basalo <albertobasalo@hotmail.com>
2020-07-13 11:39:13 +0200

    intit

--- src/old/tdd/basic/bank-client.spec.js
+++ src/old/tdd/basic/bank-client.spec.js
@@ -23,10 +23,10 @@ describe('GIVEN: a new BankClient object', () => {
     expect(actual).toEqual(expected);
   });
   test('WHEN: i make a deposit of 10 and then a new deposit of 15 THEN the last one returns the accumulate of 25', () => {
-    let input = 10;
-    sut.deposit(input);
-    input = 15;
-    const actual = sut.deposit(input);
+    const firstInput = 10;
+    sut.deposit(firstInput);
+    const secondInput = 15;
+    const actual = sut.deposit(secondInput);
     const expected = 25;
     expect(actual).toEqual(expected);
   });
